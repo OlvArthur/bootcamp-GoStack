74d92772a7ec7614b2731ee3ddb190fa3ee659ef
Arthur Oliveira <olvarthur@gmail.com>
2019-08-21 12:03:57 -0300

    Inclusão midlewares locais

--- modulo01/index.js
+++ modulo01/index.js
@@ -19,17 +19,36 @@ server.use((req, res, next) => {
   console.timeEnd('Request');
 });
 
+function checkUserExists(req, res, next) {
+  if (!req.body.name) {
+    return res.status(400).json({ error: 'Username is required ' });
+  }
+
+  return next();
+}
+
+function checkUserInArray(req, res, next) {
+  const user = users[req.params.index];
+
+  if (!user) {
+    //also possible: if (req.params.index >= users.length) {
+    return res.status(404).json({ error: 'User index invalid' });
+  }
+
+  req.user = user;
+
+  return next();
+}
+
 server.get('/users', (req, res) => {
   return res.json(users);
 });
 
-server.get('/users/:index', (req, res) => {
-  const { index } = req.params;
-
-  res.json(users[index]);
+server.get('/users/:index', checkUserInArray, (req, res) => {
+  res.json(req.user);
 });
 
-server.post('/users', (req, res) => {
+server.post('/users', checkUserExists, (req, res) => {
   const { name } = req.body;
 
   users.push(name);
@@ -37,7 +56,7 @@ server.post('/users', (req, res) => {
   return res.json(users);
 });
 
-server.put('/users/:index', (req, res) => {
+server.put('/users/:index', checkUserExists, checkUserInArray, (req, res) => {
   const { index } = req.params;
   const { name } = req.body;
 
@@ -46,7 +65,7 @@ server.put('/users/:index', (req, res) => {
   return res.json(users);
 });
 
-server.delete('/users/:index', (req, res) => {
+server.delete('/users/:index', checkUserInArray, (req, res) => {
   const { index } = req.params;
 
   users.splice(index, 1);
