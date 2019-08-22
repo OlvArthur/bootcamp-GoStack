92e5048d91f2089a4db9ef50765d7085a552230c
Arthur Oliveira <olvarthur@gmail.com>
2019-08-22 10:46:00 -0300

    Final desafio 01

--- challenge01/index.js
+++ challenge01/index.js
@@ -4,29 +4,81 @@ const server = express();
 server.use(express.json());
 
 const projects = [];
+let contRequest = 0;
+
+function checkProjectExists(req, res, next) {
+  const { id } = req.params;
+
+  const find = projects.find(function(elemento) {
+    return elemento.id === id;
+  });
+
+  if (!find) {
+    return res.status(400).json('Project not found');
+  }
+
+  return next();
+}
+
+server.use((req, res, next) => {
+  contRequest++;
+
+  console.log(`Requisições feitas até o momento: ${contRequest}`);
+
+  next();
+});
 
 server.post('/projects', (req, res) => {
-  const { id, title, tasks } = req.body;
+  const { id, title } = req.body;
+  const tasks = [];
 
-  projects.push({ id: id, title: title, tasks: tasks });
+  const project = { id, title, tasks };
+
+  projects.push(project);
 
   return res.json(projects);
-  //return res.json(`id: ${id}, title: ${title}, tasks: ${tasks}`);
 });
 
 server.get('/projects', (req, res) => {
   return res.json(projects);
 });
 
-server.put('/projects/:id', (req, res) => {
+server.put('/projects/:id', checkProjectExists, (req, res) => {
   const { id } = req.params;
   const { title } = req.body;
+
+  //const project = projects.find(item => item.id == id);
+  const find = projects.find(function(elemento) {
+    return elemento.id === id;
+  });
+
+  find.title = title;
 
   return res.json(projects);
 });
 
-server.delete('/projects/:id', (req, res) => {
-  projects.splice(id, 1);
+server.delete('/projects/:id', checkProjectExists, (req, res) => {
+  const { id } = req.params;
+
+  const find = projects.findIndex(item => item.id == id);
+  console.log(find);
+
+  projects.splice(find, 1);
+
+  return res.json(projects);
+});
+
+server.post('/projects/:id/tasks', checkProjectExists, (req, res) => {
+  const { id } = req.params;
+  const { title } = req.body;
+
+  const find = projects.find(function(elemento) {
+    return elemento.id === id;
+  });
+
+  find.tasks.push(title);
+
+  return res.json(projects);
 });
 
 server.listen(3333);
