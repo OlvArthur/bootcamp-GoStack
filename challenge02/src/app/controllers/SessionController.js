a38c8fbd7b1b70d41b2c5aaedb25e33094a6c2b2
Arthur Oliveira <olvarthur@gmail.com>
2019-08-26 21:06:06 -0300

    fim desafio 2

--- challenge02/src/app/controllers/SessionController.js
+++ challenge02/src/app/controllers/SessionController.js
@@ -5,6 +5,16 @@ import auth from '../../config/auth';
 
 class SessionController {
   async store(req, res) {
+    const schema = Yup.object().shape({
+      email: Yup.string()
+        .email()
+        .required(),
+      password: Yup.string().required(),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(401).json({ error: 'Validation fails' });
+    }
     const { email, password } = req.body;
 
     const user = await User.findOne({ where: { email } });
