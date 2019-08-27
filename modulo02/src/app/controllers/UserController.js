346771e92bf99ecfddf8748f6683f764a67f49ad
Arthur Oliveira <olvarthur@gmail.com>
2019-08-27 09:35:29 -0300

    fim modulo 02

--- modulo02/src/app/controllers/UserController.js
+++ modulo02/src/app/controllers/UserController.js
@@ -1,8 +1,22 @@
 /* eslint-disable class-methods-use-this */
+import * as Yup from 'yup';
+
 import User from '../models/User';
 
 class UserController {
   async store(req, res) {
+    const schema = Yup.object().shape({
+      name: Yup.string().required(),
+      email: Yup.string().required(),
+      password: Yup.string()
+        .required()
+        .min(6),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(401).json({ error: 'Validation fail' });
+    }
+
     const userExists = await User.findOne({ where: { email: req.body.email } });
 
     if (userExists) {
@@ -19,20 +33,47 @@ class UserController {
   }
 
   async update(req, res) {
+    const schema = Yup.object().shape({
+      name: Yup.string(),
+      email: Yup.string(),
+      oldPassword: Yup.string().min(6),
+      password: Yup.string()
+        .min(6)
+        .when('oldPassword', (oldPassword, field) =>
+          oldPassword ? field.required() : field
+        ),
+      confirmPassword: Yup.string().when('password', (password, field) =>
+        password ? field.required().oneOf([Yup.ref('password')]) : field
+      ),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(401).json({ error: 'Validation fail' });
+    }
+
     const user = await User.findOne({ where: { id: req.userId } });
 
-    const { email } = req.body;
+    const { email, oldPassword } = req.body;
 
-    user.email = email;
+    if (email !== user.email) {
+      const userExists = await User.findOne({ where: { email } });
 
-    const { id, name, email } = user;
+      if (userExists) {
+        return res.status(400).json({ error: 'User already exists' });
+      }
+    }
+
+    if (oldPassword && !(await user.checkPassword(oldPassword))) {
+      return res.status(401).json({ error: 'Password does not match' });
+    }
+
+    const { id, name, provider } = await user.update(req.body);
 
     return res.json({
-      user: {
-        id,
-        name,
-        email,
-      },
+      id,
+      name,
+      email,
+      provider,
     });
   }
 }
