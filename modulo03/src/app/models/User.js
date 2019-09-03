beb499ed90a321beae515a0a653817383cc2ee87
Arthur Oliveira <olvarthur@gmail.com>
2019-09-03 08:34:44 -0300

    Inicio modulo 03

---
+++ modulo03/src/app/models/User.js
@@ -0,0 +1,40 @@
+import Sequelize, { Model } from 'sequelize';
+import bcrypt from 'bcryptjs';
+
+class User extends Model {
+  static init(sequelize) {
+    super.init(
+      {
+        // Campos que devem ser preenchidos na criação de usuário
+        name: Sequelize.STRING,
+        email: Sequelize.STRING,
+        // .VIRTUAL : existe apenas no lado do codigo/backend
+        password: Sequelize.VIRTUAL,
+        password_hash: Sequelize.STRING,
+        provider: Sequelize.BOOLEAN,
+      },
+      {
+        sequelize,
+      }
+    );
+    // Trecho de codigo executado antes de uma ação escolhida,
+    // e.g beforeSave: antes de ser salvo
+    this.addHook('beforeSave', async user => {
+      if (user.password) {
+        user.password_hash = await bcrypt.hash(user.password, 8);
+      }
+    });
+
+    return this;
+  }
+
+  static associate(models) {
+    this.belongsTo(models.File, { foreignKey: 'avatar_id' });
+  }
+
+  checkPassword(password) {
+    return bcrypt.compare(password, this.password_hash);
+  }
+}
+
+export default User;
