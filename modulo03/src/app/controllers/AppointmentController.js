cdf85f522ceee73a96c386ac098c57d7429d1f90
Arthur Oliveira <olvarthur@gmail.com>
2019-09-12 23:15:35 -0300

    Listagem de appointments

--- modulo03/src/app/controllers/AppointmentController.js
+++ modulo03/src/app/controllers/AppointmentController.js
@@ -3,8 +3,33 @@ import * as Yup from 'yup';
 
 import Appointment from '../models/Appointment';
 import User from '../models/User';
+import File from '../models/File';
 
 class AppointmentController {
+  async index(req, res) {
+    const appointmets = await Appointment.findAll({
+      where: { user_id: req.userId, canceled_at: null },
+      order: ['date'],
+      attributes: ['id', 'date'],
+      include: [
+        {
+          model: User,
+          as: 'provider',
+          attributes: ['id', 'name'],
+          include: [
+            {
+              model: File,
+              as: 'avatar',
+              attributes: ['url', 'path', 'id'],
+            },
+          ],
+        },
+      ],
+    });
+
+    return res.json(appointmets);
+  }
+
   async store(req, res) {
     const schema = Yup.object().shape({
       provider_id: Yup.number().required(),
