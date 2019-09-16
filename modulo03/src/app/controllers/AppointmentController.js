f89177bbce8e302f2a55b11bba4cdc188e44b922
Arthur Oliveira <olvarthur@gmail.com>
2019-09-16 11:46:04 -0300

    Configuração de novas notificações com mongoDB

--- modulo03/src/app/controllers/AppointmentController.js
+++ modulo03/src/app/controllers/AppointmentController.js
@@ -1,5 +1,6 @@
-import { startOfHour, parseISO, isBefore } from 'date-fns';
 import * as Yup from 'yup';
+import { startOfHour, parseISO, isBefore, format } from 'date-fns';
+import pt from 'date-fns/locale/pt';
 
 import Appointment from '../models/Appointment';
 import User from '../models/User';
@@ -97,6 +98,18 @@ class AppointmentController {
     /**
      * Notify appointment to provider
      */
+
+    const user = await User.findByPk(req.userId);
+    const formattedDate = format(
+      hourStart,
+      "'dia' dd 'de' MMMM', às' H:mm'h'",
+      { locale: pt }
+    );
+
+    await Notification.create({
+      content: `Novo agendamento de ${user.name} para o ${formattedDate}`,
+      user: provider_id,
+    });
 
     return res.json(appointment);
   }
--- modulo03/src/app/controllers/ProviderController.js
+++ modulo03/src/app/controllers/ProviderController.js
@@ -5,12 +5,12 @@ class ProviderController {
   async index(req, res) {
     const providers = await User.findAll({
       where: { provider: true },
-      attributes: ['id', 'name', 'email', 'avatar_id'],
+      attributes: ['id', 'name', 'email'],
       include: [
         {
           model: File,
           as: 'avatar',
-          attributes: ['name', 'path', 'url'],
+          attributes: ['id', 'name', 'path', 'url'],
         },
       ],
     });
