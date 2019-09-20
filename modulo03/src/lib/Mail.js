798773854cc987269f2efae223fa2a48585c2320
Arthur Oliveira <olvarthur@gmail.com>
2019-09-19 23:36:30 -0300

    Inicio configuração de templates para emails

--- modulo03/src/lib/Mail.js
+++ modulo03/src/lib/Mail.js
@@ -1,4 +1,8 @@
 import nodemailer from 'nodemailer';
+import { resolve } from 'path';
+import exphbs from 'express-handlebars';
+import nodemailerhbs from 'nodemailer-express-handlebars';
+
 import mailConfig from '../config/mail';
 
 class Mail {
@@ -14,6 +18,26 @@ class Mail {
       //ação "auth" e then "null"
       auth: auth.user ? auth : null,
     });
+
+    this.configureTemplate();
+  }
+
+  configureTemplate() {
+    const viewPath = resolve(__dirname, '..', 'app', 'views', 'emails');
+
+    this.transporter.use(
+      'compile',
+      nodemailerhbs({
+        viewEngine: exphbs.create({
+          layoutsDir: resolve(viewPath, 'layouts'),
+          partialsDir: resolve(viewPath, 'partials'),
+          defaultLayout: 'default',
+          extname: '.hbs',
+        }),
+        viewPath,
+        extName: '.hbs',
+      })
+    );
   }
 
   /**
