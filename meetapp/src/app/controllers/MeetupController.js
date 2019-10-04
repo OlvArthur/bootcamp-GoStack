e63d42ad5d8889fce73752aa31d01aa3cb6e59bf
Arthur Oliveira <olvarthur@gmail.com>
2019-10-04 15:12:37 -0300

    Correção do bug e checagens p update meetup

--- meetapp/src/app/controllers/MeetupController.js
+++ meetapp/src/app/controllers/MeetupController.js
@@ -2,6 +2,8 @@ import Meetup from '../models/Meetup';
 import User from '../models/User';
 import File from '../models/File';
 
+import { isBefore, startOfHour, parseISO } from 'date-fns';
+
 class MeetupController {
   async store(req, res) {
     const checkManager = await User.findOne({
@@ -12,7 +14,9 @@ class MeetupController {
     });
 
     if (!checkManager) {
-      return res.json({ error: 'Only managers can create meetups' });
+      return res
+        .status(400)
+        .json({ error: 'Only managers can create meetups' });
     }
 
     /*const { originalname: name, filename: path } = req.file;
@@ -22,19 +26,47 @@ class MeetupController {
       path,
     });
 */
-    const { title, description, locale, date } = req.body;
+    const { title, description, locale, date, banner_id } = req.body;
 
-    console.log(req.userId);
+    const hourStart = startOfHour(parseISO(date));
+
+    if (isBefore(hourStart, new Date())) {
+      return res.status(400).json({ error: 'Past dates are not allowed' });
+    }
+
+    console.log(title);
     const meetup = await Meetup.create({
       title,
       description,
       locale,
       date,
       manager_id: req.userId,
-      //banner_id: file.id,
+      banner_id,
     });
 
     return res.json(meetup);
+  }
+
+  async update(req, res) {
+    const { id } = req.params;
+
+    const meetup = await Meetup.findOne({
+      where: {
+        id,
+      },
+    });
+
+    if (!meetup) {
+      return res.status(404).json({ error: "Meetup doesn't exist" });
+    }
+
+    if (meetup.manager_id !== req.userId) {
+      return res
+        .status(400)
+        .json({ error: 'You can only update meetups you manage' });
+    }
+
+    return res.json();
   }
 }
 
--- meetapp/src/routes.js
+++ meetapp/src/routes.js
@@ -20,6 +20,8 @@ routes.use(authMiddleware);
 routes.put('/users', UserController.update);
 
 routes.post('/files', upload.single('file'), FileController.store);
+
 routes.post('/meetups', MeetupController.store);
+routes.put('/meetups/:id', MeetupController.update);
 
 export default routes;
