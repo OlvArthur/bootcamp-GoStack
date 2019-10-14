115d9b32b4fd0023fd2012d28203b3c2aed754a6
Arthur Oliveira <olvarthur@gmail.com>
2019-10-14 19:18:06 -0300

    Fim desafio 3

--- meetapp/src/app/controllers/ScheduleController.js
+++ meetapp/src/app/controllers/ScheduleController.js
@@ -7,14 +7,15 @@ import User from '../models/User';
 class ScheduleController {
   async index(req, res) {
     //Método que checa as listagens do manager de um determinado dia
-    const checkManager = await User.findOne({
+    /*const checkManager = await User.findOne({
       where: { id: req.userId, manager: true },
     });
 
     if (!checkManager) {
       return res.status(401).json({ error: 'You must be a manager' });
     }
-    const { pages = 1 } = req.query;
+    */
+    const { page = 1 } = req.query;
 
     const { date } = req.query;
     const parsedDate = parseISO(date);
@@ -28,7 +29,14 @@ class ScheduleController {
       },
       order: ['date'],
       limit: 20,
-      offset: (pages - 1) * 20,
+      include: [
+        {
+          model: User,
+          as: 'manager',
+          attributes: ['name', 'email'],
+        },
+      ],
+      offset: (page - 1) * 20,
     });
 
     return res.json(schedule);
