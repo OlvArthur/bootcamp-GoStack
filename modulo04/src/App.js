7a4ef093ce6925712473dac38939a35b5e23e733
Arthur Oliveira <olvarthur@gmail.com>
2019-10-21 19:53:10 -0300

    Configuração loaders css e primeira estilização

--- modulo04/src/App.js
+++ modulo04/src/App.js
@@ -1,6 +1,8 @@
 //Qualquer arquivo que precisa utilizar JSX, precisa importar o react
 import React from 'react';
 
+import './App.css';
+
 function App() {
   return <h1>Hello Arthur</h1>;
 }
--- modulo04/webpack.config.js
+++ modulo04/webpack.config.js
@@ -20,7 +20,13 @@ module.exports = {
       {
         test: /\.js$/,
         exclude: /node_modules/,
-        use: 'babel-loader',
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+      {
+        test: /\.css$/,
+        use: [{ loader: 'style-loader' }, { loader: 'css-loader' }],
       },
     ],
   },
