e500479310094c96663f1bd94312f32b264d4be7
Arthur Oliveira <olvarthur@gmail.com>
2019-10-23 15:53:51 -0300

    inicio desafio React(4)

---
+++ desafioReact01/.eslintrc.js
@@ -0,0 +1,26 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+  },
+  extends: ['airbnb'],
+  plugins: ['react', 'prettier'],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  rules: {
+    'prettier/prettier': 'error',
+    'class-methods-use-this': 'on',
+    'no-param-reassign': 'off',
+    camelcase: 'off',
+    'no-unused-vars': ['error', { argsIgnorePattern: 'next' }],
+  },
+};
---
+++ desafioReact01/src/components/Layout.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+import Header from './Header';
+
+class Layout extends Component {
+  render() {
+    return (
+      <h1>
+        <Header />
+      </h1>
+    );
+  }
+}
+
+export default Layout;
