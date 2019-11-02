ef1fda5740a4a8b5b8ebe510f9d84b44a1296c98
Arthur Oliveira <olvarthur@gmail.com>
2019-11-02 10:00:43 -0300

    Estilização página main

--- modulo05/src/pages/Main/index.js
+++ modulo05/src/pages/Main/index.js
@@ -1,9 +1,34 @@
 import React from 'react';
 
-import { Title } from './styles';
+import { FaGithubAlt, FaPlus } from 'react-icons/fa';
 
-function Main() {
-  return <Title>Hello World</Title>;
+import { Container, Form, SubmitButton } from './styles';
+
+export default function Main() {
+  return (
+    /** Em casos de 2 ou mais chaveamentos, é recomendado
+     * sempre criar um novo componente estilizado, ao invés de um html puro
+     */
+
+    /**
+     * No caso do <Form> será necessário estlizar o componente de acordo com a
+     * propriedade. Sempre que isso ocorrer, esse elemento precisa
+     * obrigatoriamente virar um novo componente dentro do styled components
+     */
+
+    <Container>
+      <h1>
+        <FaGithubAlt />
+        Repositórios
+      </h1>
+
+      <Form onSubmit={() => {}}>
+        <input type="text" placeholder="Adicionar repositorio" />
+
+        <SubmitButton>
+          <FaPlus color="#FFF" size={14} />
+        </SubmitButton>
+      </Form>
+    </Container>
+  );
 }
-
-export default Main;
--- modulo05/src/pages/Main/styles.js
+++ modulo05/src/pages/Main/styles.js
@@ -1,5 +1,64 @@
 import styled from 'styled-components';
 
-export const Title = styled.h1`
-  color: #fff;
+export const Container = styled.div`
+  max-width: 700px;
+  background: #fff;
+  border-radius: 4px;
+  box-shadow: 0 0 20px rgba(0, 0, 0, 0.3);
+
+  /* Ajuda a ajustar o elemento interior  */
+  padding: 30px;
+
+  /* Ajuda a ajustar o elemento na página.
+  * Primeiro elemento vertical, segundo horizontal */
+  margin: 80px auto;
+
+  h1 {
+    font-size: 20px;
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+
+    svg {
+      margin-right: 10px;
+    }
+  }
+`;
+
+export const Form = styled.form`
+  /* As três caracteristicas garantem que os elementos, fiquem
+  um ao lado do outro independente do tamanho */
+  margin-top: 30px;
+  display: flex;
+  flex-direction: row;
+
+  input {
+    /*Faz o elemento ocupar todo espaço disponível */
+    flex: 1;
+
+    border: 1px solid #eee;
+
+    /* Mesma função do margin, porém entre os elementos e a borda interna */
+    padding: 10px 15px;
+
+    border-radius: 4px;
+    font-size: 16px;
+  }
+`;
+
+/* .attrs permite passar qualuqer tipo de atributo ao elemento através do css */
+
+export const SubmitButton = styled.button.attrs({
+  type: 'submit',
+})`
+  background: #7159c1;
+  border: 0;
+  padding: 0px 15px;
+  margin-left: 10px;
+  border-radius: 4px;
+
+  /** garantem que o conteudo do botão sempre esteja alinhado ao centro */
+  display: flex;
+  justify-content: center;
+  align-items: center;
 `;
